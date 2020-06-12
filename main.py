e0c72f351a151bfc968d04dc6bd1cab31cf5a3a3
Godson <lgodsongnanaraj@gmail.com>
2020-06-12 09:39:00 +0530

    Added exception handling and header to request

--- main.py
+++ main.py
@@ -1,44 +1,57 @@
+import os
 import sys
 from bs4 import BeautifulSoup as BS
-from urllib.request import urlretrieve
-mods = {"%2f": "/", "%3a": ":", "%2520": " "}
+import urllib.request as urq
+from urllib.parse import unquote
+from urllib.error import HTTPError
 
 
-def pretty(html_file):
-    fp = open(html_file, "r")
-    html_text = fp.read()
-    fp.close()
-
-    soup = BS(html_text, "html.parser")
-    html = soup.prettify()
-    fp = open("pretty_google.html", "w")
-    fp.write(html)
-    fp.close()
+def build_user_agent():
+    agent = urq.build_opener()
+    agent.addheaders = [
+        ('User-agent',
+         'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.1')
+    ]
+    urq.install_opener(agent)
 
 
-def get_images(url_file):
+def get_images(url_file, prefix, start_no):
     fp = open(url_file, "r")
     urls = fp.readlines()
-    for i, url in enumerate(urls):
-        print(urlretrieve(url, filename="image"+str(i)))
-        break
+    build_user_agent()
+
+    if not os.path.exists("downloads"):
+        os.mkdir("downloads")
+    prefix = "downloads/" + prefix
+
+    for url in urls:
+        url = url[:-1]
+        print("Downloading image from ", url)
+        filename = prefix + str(start_no)
+        try:
+            urq.urlretrieve(url, filename=filename)
+            print("Downloaded ", filename)
+            start_no += 1
+        except HTTPError as e:
+            print(e)
+            print("Error on dowloading ", url)
 
 
 def get_img_link(url):
     murl = url.split("mediaurl=")[1]
     img = murl.split("&")[0]
-    for key, value in mods.items():
-        img = img.replace(key, value)
-        img = img.replace(key.upper(), value)
-    fp = open("urls.txt", "a")
-    fp.write(img)
+    return unquote(img)
 
 
 def parse_href(links, key, prefix):
+    print("Parsing links and storing it in \"urls.txt\"")
+    fp = open("urls.txt", "w")
     for link in links:
         href = link["href"]
         if key in href:
-            get_img_link(prefix+href)
+            url = get_img_link(prefix+href)
+            fp.write(url+"\n")
+    print("parsing urls completed")
 
 
 def get_img_index_links(html_file):
@@ -47,13 +60,8 @@ def get_img_index_links(html_file):
     fp.close()
     soup = BS(html_text, "html.parser")
     links = soup.find_all("a")
-    google = soup.find("base")
-    if google:
-        key = "/imgres/imgurl?"
-        prefix = "https://www.google.com"
-    else:
-        key = "/images/search?view"
-        prefix = "https://www.bing.com"
+    key = "/images/search?view"
+    prefix = "https://www.bing.com"
     parse_href(links, key, prefix)
 
 
@@ -63,6 +71,5 @@ if __name__ == "__main__":
         sys.exit(0)
 
     html_file = sys.argv[1]
-    # pretty(html_file)
-    # get_img_index_links(html_file)
-    get_images(html_file)
+    get_img_index_links(html_file)
+    get_images("urls.txt", "image", 1)
